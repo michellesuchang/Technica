bb3d68be5160119d41b7f300d699e02b464b9e4a
raisameneses <raisameneses1@gmail.com>
2017-11-04 17:12:19 -0700

    raisa no image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Global S.O.S';
+  subtitle = 'Together for the world';
 }
